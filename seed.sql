8ed814adabeca71c5e121c1840257b3b42186cc5
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-29 15:07:39 -0800

    inserted more data to the tables

--- seed.sql
+++ seed.sql
@@ -6,3 +6,6 @@ VALUES ("Accountant",70000,1),("Recruiter", 80000, 2),("Engineer",90000,3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Tom","Wood",1,1),("Mike","Hard",2,1),("Mary","May",3,2),("John","Duke",3,2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ryan","Haralson",1,1),("Shannon","Weiler",2,1),("Renate","Wharton",3,2),("Margaret","Young",3,2)("Laurie","Paynter",3,2)("Robert","Hill",3,2);
