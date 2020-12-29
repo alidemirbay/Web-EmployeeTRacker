81c862b1f1ce4715a3dae4d4e6304cf3d79ce0ac
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-29 15:38:49 -0800

    added some questions for for start sunction

--- app.js
+++ app.js
@@ -16,3 +16,34 @@ connection.connect(function (err) {
     if (err) throw err;
     start();
 });
+
+function start() {
+
+    inquirer.prompt({
+        type: "list",
+        name: "action",
+        message: "Choose below.",
+        choices: [
+            "View All Employees",
+            "View All Employees by Department",
+            "View All Employees by Role",
+            "Quit",
+        ]
+    }).then(function (answer) {
+        switch (answer.action) {
+            case "View All Employees": viewAll();
+                break;
+            case "View All Employees by Department": viewAllDepartment();
+                break;
+            case "View All Employees by Role": viewAllRole();
+                break;
+            case "Quit": connection.end();
+                break;
+        }
+    });
+
+
+
+
+
+}
