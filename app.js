0af9f894edd514d19a880091857c2401a19f3013
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-30 10:56:16 -0800

    added function to view employees by department

--- app.js
+++ app.js
@@ -22,28 +22,61 @@ function start() {
     inquirer.prompt({
         type: "list",
         name: "action",
-        message: "Choose below.",
-        choices: [
-            "View All Employees",
-            "View All Employees by Department",
-            "View All Employees by Role",
-            "Quit",
-        ]
+        message: "What would you like to do?",
+        choices: ["View All Employees", "View Employees by Department", "View Employees by Role", "Quit",]
     }).then(function (answer) {
         switch (answer.action) {
-            case "View All Employees": viewAll();
+            case "View All Employees": viewAllEmployees();
                 break;
-            case "View All Employees by Department": viewAllDepartment();
+            case "View Employees by Department": viewByDepartment();
                 break;
-            case "View All Employees by Role": viewAllRole();
+            case "View Employees by Role": viewByRole();
                 break;
             case "Quit": connection.end();
                 break;
         }
     });
+}
 
 
+function viewAllEmployees() {
+    connection.query("SELECT employee.id,first_name, last_name, role.salary, role.title, department.name as department_name  FROM employee   INNER JOIN role ON employee.role_id = role.id    INNER JOIN department ON role.department_id = department.id",
 
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            start();
+        });
+}
 
+function viewByDepartment() {
+    connection.query("SELECT name FROM department",
+        function (err, res) {
+            if (err) throw err;
 
+            inquirer.prompt([
+                {
+                    name: "choice", type: "list",
+                    choices: function () {
+                        var choiceArr = [];
+                        for (var i = 0; i < res.length; i++) { choiceArr.push(res[i].name); }
+                        return choiceArr;
+                    },
+                    message: "Which Department?",
+                },
+            ])
+                .then(function (answer) {
+                    // console.log(answer);
+                    // console.log(answer.choice);
+
+                    connection.query("SELECT first_name, last_name, role.salary, role.title, department.name as department FROM employee INNER JOIN role ON employee.role_id = role.id   INNER JOIN department ON role.department_id = department.id WHERE department.name = ? ", answer.choice,
+
+                        function (err, res) {
+                            if (err) throw err;
+                            console.table(res);
+                            start();
+                        });
+                });
+        }
+    );
 }
--- seed.sql
+++ seed.sql
@@ -5,7 +5,14 @@ INSERT INTO role(title, salary, department_id)
 VALUES ("Accountant",70000,1),("Recruiter", 80000, 2),("Engineer",90000,3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom","Wood",1,1),("Mike","Hard",2,1),("Mary","May",3,2),("John","Duke",3,2);
+VALUES ("Tom","Wood",1,null),
+("Ryan","Haralson",1,1),
+("Shannon","Weiler",2,1),
+("Mike","Hard",2,1),
+("Mary","May",3,null),
+("John","Duke",3,5),
+("Renate","Wharton",3,5),
+("Margaret","Young",3,5),
+("Laurie","Paynter",3,5),
+("Robert","Hill",3,5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ryan","Haralson",1,1),("Shannon","Weiler",2,1),("Renate","Wharton",3,2),("Margaret","Young",3,2)("Laurie","Paynter",3,2)("Robert","Hill",3,2);
